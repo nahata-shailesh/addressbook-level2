0535f155db237f753283922b9baabd513ae56f10
nahata-shailesh <nahata.shailesh@gmail.com>
2016-09-07 19:11:12 +0800

    implement inheritance using Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,26 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+	protected String value;
+    protected boolean isPrivate;
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+    
+    public String getValue() {
+    	return this.value;
+    }
+
+}
